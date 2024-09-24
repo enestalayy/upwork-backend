4ca9e9973fdb1eb4c694dda3189ea964d307a791
Enes Talay <enestalayy@gmail.com>
2024-09-24 17:25:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,28 +25,33 @@ mongoose
   .catch((err) => console.error("MongoDB bağlantı hatası:", err));
 
 app.get("/api/cron", async (req, res) => {
-  try {
-    const users = await User.find({});
-    for (let user of users) {
-      for (let filter of user.filters) {
-        const scrapedJobs = await scrapeJobList(filter.url);
-        const savedJobs = await Promise.all(
-          scrapedJobs.map(async (jobData) => {
-            const newJob = new Job(jobData);
-            await newJob.save();
-            return newJob._id;
-          })
-        );
-        filter.jobs = savedJobs;
-        await user.save();
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).end("Unauthorized");
+  } else
+    try {
+      const users = await User.find({});
+      for (let user of users) {
+        for (let filter of user.filters) {
+          const scrapedJobs = await scrapeJobList(filter.url);
+          const savedJobs = await Promise.all(
+            scrapedJobs.map(async (jobData) => {
+              const newJob = new Job(jobData);
+              await newJob.save();
+              return newJob._id;
+            })
+          );
+          filter.jobs = savedJobs;
+          await user.save();
+        }
       }
+      console.log("Tüm kullanıcılar için işler scrape edildi ve kaydedildi.");
+      res.status(200).json({ message: "Cron job başarıyla çalıştı" });
+    } catch (error) {
+      console.error("Cron job hatası:", error);
+      res.status(500).json({ error: "Sunucu hatası" });
     }
-    console.log("Tüm kullanıcılar için işler scrape edildi ve kaydedildi.");
-    res.status(200).json({ message: "Cron job başarıyla çalıştı" });
-  } catch (error) {
-    console.error("Cron job hatası:", error);
-    res.status(500).json({ error: "Sunucu hatası" });
-  }
 });
 
 // Yeni filter oluşturma endpoint'i
