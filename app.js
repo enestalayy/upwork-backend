6fdca5fbbd1436b5424ac362897d39f40a1e26cf
Enes Talay <enestalayy@gmail.com>
2024-10-19 07:14:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Bir hata oluştu", error: err.message });
 });
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 8000;
-  app.listen(PORT, () => console.log(`Server ${PORT} portunda çalışıyor`));
-}
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 module.exports = app;
